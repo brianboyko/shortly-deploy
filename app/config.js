2039f27c145b8f3888e25566b1fd9a3c84933aa0
Brian Boyko <brian.boyko@gmail.com>
2015-08-26 18:43:29 -0500

    revert to sqlite

--- app/config.js
+++ app/config.js
@@ -3,14 +3,14 @@ var Bookshelf = require('bookshelf');
 var path = require('path');
 
 var db = Bookshelf.initialize({
-  client: 'pg',
+  client: 'sqlite3',
   connection: {
     host: '127.0.0.1',
-    port: '5432',
-    user: 'yprtkvpfgxecup',
-    password: 'fX86nI-x0BrBSGpn0AANYzD3Pq',
-    database: 'd6ahruqmk049om',
-    charset  : 'UTF8_GENERAL_CI'
+    user: 'your_database_user',
+    password: 'password',
+    database: 'shortlydb',
+    charset: 'utf8',
+    filename: path.join(__dirname, '../db/shortly.sqlite')
   }
 });
 
